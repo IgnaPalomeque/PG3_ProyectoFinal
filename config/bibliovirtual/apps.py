4d04bb746ba99e4d6668847780402764863b2393
IgnaPalomeque <53220867+IgnaPalomeque@users.noreply.github.com>
2020-10-30 16:35:07 -0300

    Correccion de errores este si

---
+++ config/bibliovirtual/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BibliovirtualConfig(AppConfig):
+    name = 'bibliovirtual'
--- config/config/settings.py
+++ config/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1)j-#u6u4u01=&kv#_3yc(8q$-@*k4fnucihc6b%)w9hnh8^-s'
+SECRET_KEY = 'i91lo!*giz@6m1)t33(fj%$bbn2c*(q)xi$@74b)lfu_^*8f5q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,14 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'biblioescolar',
 ]
 
 MIDDLEWARE = [
