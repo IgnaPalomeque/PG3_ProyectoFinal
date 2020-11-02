077be5b37dbc8ee9c29c3c5b308a70986375f838
GinoPepi <pepi.gino@gmail.com>
2020-11-02 15:58:01 -0300

    Creacion de clases y admin para pruebas

--- config/bibliovirtual/admin.py
+++ config/bibliovirtual/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class ProfesorAdmin (admin.ModelAdmin):
+    fieldsets = (
+        ('Datos', {
+            'fields': ('apellido','nombre','dni')
+        }),
+    )
+    search_fields = ['apellido','dni']
+    list_display = ['apellido','nombre','dni','cursos' ]
+    list_display_links = ['apellido','nombre','dni','cursos']
+
+class AlumnoAdmin (admin.ModelAdmin):
+    fieldsets = (
+        ('Datos', {
+            'fields': ('apellido','nombre','dni','curso')
+        }),
+    )
+    search_fields = ['apellido','dni']
+    list_display = ['apellido','nombre','dni','curso']
+    list_display_links = ['apellido','nombre','dni','curso']
+
+class LibroEnVentaAdmin (admin.ModelAdmin):
+    list_display = ('titulo','materia', 'descripcion')
+
+class MateriaDescargableAdmin (admin.ModelAdmin):
+    list_display = ('titulo','materia',)
+
+admin.site.register (MateriaDescargable,MateriaDescargableAdmin,)
+admin.site.register (LibroEnVenta,LibroEnVentaAdmin,)
+admin.site.register (Alumno,AlumnoAdmin,)
+admin.site.register (Profesor,ProfesorAdmin,)
--- config/bibliovirtual/models.py
+++ config/bibliovirtual/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Curso (models.Model):
+    divisiones = [
+        ("A","A"), 
+        ("B","B"),
+        ("C","C"),
+    ]
+    division = models.CharField(max_length= 1, choices = divisiones,)
+
+class Persona (models.Model):
+    dni = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length= 10)
+    apellido = models.CharField(max_length= 10)
+    contraseña = models.CharField(max_length= 20)
+
+class Alumno (Persona):
+    curso = models.ForeignKey('Curso', on_delete=models.CASCADE, null=True,)
+
+class Profesor (Persona):
+    cursos = []
+
+class Materia (models.Model):
+    titulo = models.CharField(max_length= 20)
+    
+class Material (models.Model):
+    materias = [
+        ("L","Lengua"), 
+        ("B", "Biologia"),
+        ("M", "Matematica"),
+        ("I", "Ingles"),
+        ("P", "Programacion"),
+    ]
+    materia = models.CharField(max_length= 20,choices = materias)
+    titulo = models.CharField(max_length= 20,primary_key=True)
+    curso = models.ForeignKey('Curso', on_delete=models.CASCADE, null=True,)
+
+class MateriaDescargable (Material):
+    # pdf = models.FileFields(upload_to='pdfdescarga/')
+    pass
+
+class LibroEnVenta (Material):
+    vendedor = models.ForeignKey('Alumno', on_delete=models.CASCADE, null=True,)
+    descripcion = models.CharField(max_length= 200)
